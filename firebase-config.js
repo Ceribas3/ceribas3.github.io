776a3b49be2da4ec6a113818d06b86b4b1977917
Ceribas3 <cceribas3@gmail.com>
2025-04-17 21:19:10 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD5L93RbgyI2voaite89sy87Mp3KwHqBEQ",
+    authDomain: "ceribas3-saite.firebaseapp.com",
+    projectId: "ceribas3-saite",
+    storageBucket: "ceribas3-saite.firebasestorage.app",
+    messagingSenderId: "728919586798",
+    appId: "1:728919586798:web:92265e806f32b885f780ff",
+    measurementId: "G-2204JGLHCM"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
